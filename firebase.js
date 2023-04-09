d23b1dcf3fa2b5b4235e0744706197b5deda0cc2
Vitalik_Lopatiuk <workingprogram11@gmail.com>
2023-04-09 16:47:28 +0300

    API

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider } from "firebase/auth";
 export const provider = new GoogleAuthProvider();
 const firebaseConfig = {
-  apiKey: 'AIzaSyA1pLi2l_QIi6LK5A1PDDJ-yegIdYjo1uA',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
