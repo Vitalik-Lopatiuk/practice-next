335bfe146d4d39d80d7d0f742e22ab258ffdf35c
Vitalik_Lopatiuk <workingprogram11@gmail.com>
2023-04-09 17:47:54 +0300

    sd

--- components/form.jsx
+++ components/form.jsx
@@ -3,18 +3,21 @@ import { useForm } from "react-hook-form";
 import { Button } from "@mui/material";
 import Image from "next/image";
 import line from "../public/form/Line.svg";
-import google from '../public/form/Google.svg'
-import facebook from '../public/form/Facebook.svg'
+import google from "../public/form/Google.svg";
+import facebook from "../public/form/Facebook.svg";
 
-const Form = ({ handleRegOrLog, text }) => {
+const Form = ({ text }) => {
   const { register, handleSubmit } = useForm();
+  const asa = (data) => {
+    alert(data);
+  };
   return (
     <div className="flex  font-poppins h-screen bg-gradient-to-r from-[rgba(146,179,176,1)] via-[ rgba(62,149,171,1)] to-[rgba(38,110,154,1)]  justify-center items-center flex-col gap-6">
       <h1 className="text-6xl  font-bold tracking-widest">WELCOME</h1>
 
       <form
         className="flex gap-8 flex-col text-white "
-        onSubmit={handleSubmit(handleRegOrLog)}
+        onSubmit={handleSubmit(asa)}
       >
         <input
           placeholder="email"
@@ -41,7 +44,6 @@ const Form = ({ handleRegOrLog, text }) => {
         >
           {text}
         </Button>
-
       </form>
       <div className="flex gap-3 ">
         <p>{text === "login" ? "Don’t have account?" : "Have account"}</p>
@@ -56,12 +58,12 @@ const Form = ({ handleRegOrLog, text }) => {
       <div className="flex gap-4">
         <Image alt="line" src={line} />
         <p>OR LOGIN WITH</p>
-        <Image alt="line" src={line} /> 
+        <Image alt="line" src={line} />
       </div>
 
       <div className="flex gap-4">
-      <Image  alt="google sinIn" src={google} />
-      <Image alt="facebook sinIn" src={facebook} />
+        <Image alt="google sinIn" src={google} />
+        <Image alt="facebook sinIn" src={facebook} />
       </div>
     </div>
   );
