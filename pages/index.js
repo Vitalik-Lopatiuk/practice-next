7b14ea201d9e725646ad3c29b527704e4797e2c2
Vitalik_Lopatiuk <workingprogram11@gmail.com>
2023-04-09 17:06:50 +0300

    API

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,21 @@ import { Button } from "@mui/material";
 import Link from "next/link";
 
 export default function Home() {
-  const { isAuth } = useAuth();
-  return isAuth ? (
+  // const { isAuth } = useAuth();
+  return (
     <div className="font-concert">Home:</div>
-  ) : (
-    <div className=" flex font-concert justify-center items-center flex-col h-screen">
-      <div>
-        <h1>YOU ARE NOT AUTHORIZED</h1>
-      </div>
+  )
+    //  : (
+  //   <div className=" flex font-concert justify-center items-center flex-col h-screen">
+  //     <div>
+  //       <h1>YOU ARE NOT AUTHORIZED</h1>
+  //     </div>
 
-      <div>
-        <Button variant="contained">
-          <Link href="/login">go to Login page</Link>
-        </Button>
-      </div>
-    </div>
-  );
+    //   <div>
+    //     <Button variant="contained">
+    //       {/* <Link href="/login">go to Login page</Link> */}
+    //     </Button>
+    //   </div>
+    // </div>
+  // );
 }
