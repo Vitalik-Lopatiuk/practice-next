95feee84e3fd4e2bc1fe9d0f2f07ab59c1694373
Vitalik_Lopatiuk <workingprogram11@gmail.com>
2023-04-09 19:41:02 +0300

    a

--- components/singUp.jsx
+++ components/singUp.jsx
@@ -1,11 +1,25 @@
 import Form from "./form";
-import useSingUpWithEmail from "@/hook/useSingUpWithEmail";
-import useSingInWithGoogle from "@/hook/useSingInWithGoogle";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { setUser } from "@/store/userSlice";
+import { useRouter } from "next/router";
 
 const SingUp = () => {
-  const { createUser } = useSingUpWithEmail();
-  // const { createUserWithGoogle } = useSingInWithGoogle();
-
+  const auth = getAuth();
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const createUser = ({ email, password }) => {
+    createUserWithEmailAndPassword(auth, email, password).then(({ user }) => {
+      dispatch(
+        setUser({
+          email: user.email,
+          id: user.uid,
+          token: user.accessToken,
+        })
+      );
+      router.push("/");
+    });
+  };
   return (
     <div>
       <Form
