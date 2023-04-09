2e2ca979d19a704f4f74eefaa433b9f1c8c3cdbe
Vitalik_Lopatiuk <workingprogram11@gmail.com>
2023-04-09 17:12:13 +0300

    A

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import SingIn from "@/components/singIn";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      {/* <SingIn /> */}
+      log
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import SingUp from '@/components/singUp'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      {/* <SingUp/> */}
+      reg
+    </div>
+  )
+}
+
+export default Register
